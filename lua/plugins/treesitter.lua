6ac037b64b8959b38bfd43e5f9738ec6f23382b6
Markus Ullmann <morjgus@protonmail.com>
2025-11-06 12:57:37 +0100

    Add additional treesitter parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python", "latex", "css", "zig", "rust", "mlir", "jinja", "cmake", "csv", "regex", "norg", "scss", "svelte", "tsx", "typst", "vue"},
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
