dc689684de8f1707c6cd0e50467174b83ddf3fb9
Markus Ullmann <morjgus@protonmail.com>
2026-05-12 14:53:16 +0200

    Autoformat md files with mdformat
    Automatically format md files on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,4 +1,19 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {},
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    formatters_by_ft = {
+      markdown = { "mdformat" },
+    },
+    formatters = {
+      mdformat = {
+        prepend_args = { "--number", "--wrap", "100", "--end-of-line", "lf" },
+      },
+    },
+    format_on_save = {
+      timeout_ms = 2000,
+      lsp_format = "fallback",
+    },
+  },
 }
